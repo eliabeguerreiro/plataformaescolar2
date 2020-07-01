cce14c11d97bb8129970e6d51312057a6440e0c2
Eliabe <eliabepaz.wprk@gmail.com>
2020-07-01 16:49:15 -0300

    sistema de conexão e validação de loguin incompleto

---
+++ sistem/conexao.php
@@ -0,0 +1,6 @@
+<?php
+define('HOST', '127.0.0.1:3308');
+define('USUARIO', 'root');
+define('SENHA', '');
+define('DB', 'plataformaescolar2');
+$conn = mysqli_connect(HOST, USUARIO, SENHA, DB) or die ('Não foi possível conectar');
---
+++ sistem/verifica-login.php
@@ -0,0 +1,37 @@
+<?php
+session_start();
+include("conexao.php");
+$btnLogin = filter_input(INPUT_POST, 'btnLogin', FILTER_SANITIZE_STRING);
+if($btnLogin){
+	$matricula = filter_input(INPUT_POST, 'matricula', FILTER_SANITIZE_STRING);
+	$senha = filter_input(INPUT_POST, 'senha', FILTER_SANITIZE_STRING);
+	if((!empty($matricula)) AND (!empty($senha))){
+		$result_usuario = "SELECT id, nome, matricula, senha, tipo FROM usuarios WHERE matricula='$matricula' LIMIT 1";//talvez isso vá mudar
+		$resultado_usuario = mysqli_query($conn, $result_usuario);
+		if($resultado_usuario){
+			$row_usuario = mysqli_fetch_assoc($resultado_usuario);
+			ob_start();
+			if(password_verify($senha, $row_usuario['senha'])){
+				$_SESSION['id'] = $row_usuario['id'];
+				$_SESSION['nome'] = $row_usuario['nome'];
+				$_SESSION['matricula'] = $row_usuario['matricula'];
+				$_SESSION['tipo'] = $row_usuario['tipo'];//isso vai musar
+				$nh = '';
+    			$nh .='Dia: '. date('d/m/Y') .' Hora: '.date('h:i:s');
+				$relat = "INSERT INTO `entrada` (`matricula`, `entrada`) VALUES ('" .$_SESSION['matricula']. "','$nh')";
+				$resultado_usario = mysqli_query($conn, $relat);
+				header("Location:../src.php?pg=painel");//isso vai mudar
+			}else{
+				$_SESSION['msg'] = "Login e senha incorreto!";
+				header("Location: ../index.php");//isso vai mudar
+			}
+		}
+	}else{
+		$_SESSION['msg'] = "Você precisar inserir os dados de login!";
+		header("Location: ../index.php");//isso vai mudar
+	}
+}else{
+	$_SESSION['msg'] = "Página não encontrada";
+	header("Location: ../index.php");//isso vai mudar
+}
+ob_end_flush();
