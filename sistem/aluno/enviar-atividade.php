4b52ae9c986b0c3f884e8fcfea972ce63b2d7ea5
Eliabe <eliabepaz.wprk@gmail.com>
2020-07-17 16:38:07 -0300

    envio de atividade do aluno quase pronto

--- sistem/aluno/enviar-atividade.php
+++ sistem/aluno/enviar-atividade.php
@@ -1,12 +1,25 @@
 <?php
 session_start();
 include("../conexao.php");
-
 var_dump($_POST);
-
-if(isset($_GET['id'])? $_GET['id'] : '#'){}
-$id = $_GET['id'];
-$_SESSION['id'] = $id;
-
-
 var_dump($_SESSION);
+
+$mac =  "SELECT titulo FROM `atividade-prof` WHERE id = '" .$_POST['atividade']. "'";
+$mach = mysqli_fetch_array(mysqli_query($conn, $mac));
+//var_dump($mach);
+
+
+$fil =  "SELECT max(fila) FROM `atividade-aluno`";
+$fila = mysqli_fetch_array(mysqli_query($conn, $fil));
+var_dump($fila);
+$fila['max(fila)'] +=1;
+
+
+$sql = "INSERT INTO `atividade-aluno` VALUES ('" .$mach['titulo']. "', '" .$_POST['link']. "', '" .$_SESSION['dados-aluno']['cod-turma']. "', '" .$_SESSION['nome']. "', '" .$_SESSION['disciplina']. "', '" .$fila['max(fila)']. "')";
+echo $sql;
+if (mysqli_query($conn, $sql)){
+    $_SESSION['msg']='Enviado com Sucesso';
+    header("Location: index.php?area=painel-disciplina");
+}else{
+    echo"deu negado";
+}
--- sistem/aluno/index.php
+++ sistem/aluno/index.php
@@ -4,12 +4,19 @@ include("../conexao.php");
 $sql = "SELECT * FROM `dados-aluno` WHERE matricula = '" .$_SESSION['matricula']. "'";
 //echo $sql;
 $dadosAluno = mysqli_fetch_array(mysqli_query($conn, $sql));
+$_SESSION['dados-aluno'] = $dadosAluno;
 //var_dump($dadosAluno);
 $pagina=isset($_GET['area'])? $_GET['area'] : 'painel';
 
 
 
 if ($pagina == 'dados'){
+    if(isset($_SESSION['msg'])){
+        echo "<div class='alert alert-danger' role='alert'>";
+        echo($_SESSION['msg']);
+        echo"</div>";
+        unset($_SESSION['msg']);
+}
     ?>
 <div id="content">
 
@@ -128,6 +135,12 @@ if ($pagina == 'dados'){
 
 
 if($pagina == 'painel'){
+    if(isset($_SESSION['msg'])){
+        echo "<div class='alert alert-danger' role='alert'>";
+        echo($_SESSION['msg']);
+        echo"</div>";
+        unset($_SESSION['msg']);
+}
     ?>
 
 <div id="content">
@@ -241,6 +254,12 @@ if($pagina == 'painel'){
 }
 
 if ($pagina == 'painel-turma'){
+    if(isset($_SESSION['msg'])){
+        echo "<div class='alert alert-danger' role='alert'>";
+        echo($_SESSION['msg']);
+        echo"</div>";
+        unset($_SESSION['msg']);
+}
     ?>
 
 <div id="content">
@@ -352,6 +371,12 @@ if ($pagina == 'painel-turma'){
 }
 
 if ($pagina == 'painel-disciplina'){
+    if(isset($_SESSION['msg'])){
+        echo "<div class='alert alert-danger' role='alert'>";
+        echo($_SESSION['msg']);
+        echo"</div>";
+        unset($_SESSION['msg']);
+}
     //o nome da disciplina tem que vir do link gerado na pagina anterior 
 $disciplina = "disciplinateste01";
 $_SESSION['disciplina'] =  $disciplina;
@@ -392,7 +417,10 @@ $_SESSION['disciplina'] =  $disciplina;
                             class="sr-only">(current)</span></a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="enviar-atividade.php?">Enviar Atividade</a>
+                    
+                <button type="button" class="btn btn-warning" data-toggle="modal" data-target="#exampleModal">Enviar Atividade</button>
+
+
                 </li>
                 <li class="nav-item">
                     <a class="nav-link" href="">Profile</a>
@@ -415,7 +443,64 @@ $_SESSION['disciplina'] =  $disciplina;
         </div>
     </nav>
 
-            
+            <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLabel">Enviar respostas</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+                </button>
+                
+            </div>
+            <div class="modal-body">
+                <form method="POST" action ="enviar-atividade.php">
+                <div class="form-group">
+                    <label for="atividade" class="col-form-label">Atividade:</label>
+                    <select name="atividade">
+                    <option value="">Escolha a atividade</option>
+                    <?php
+                    $atv = "SELECT id FROM `atividade-prof`";
+                    //echo($atv);
+                    $num = mysqli_num_rows(mysqli_query($conn,$atv));
+                    $ati = mysqli_fetch_array(mysqli_query($conn, $atv));
+                    //var_dump($ati);
+                    //var_dump($num);
+                    $n = $num;
+                    $n += 20;
+                    while($n != 0){                   
+                        $query = "SELECT id, titulo FROM `atividade-prof` WHERE turma = '" .$dadosAluno['cod-turma']. "' AND disciplina = '" .$disciplina. "' AND fila = '" .$n. "'";
+                        //echo $sql;
+                        $ativ = mysqli_fetch_array(mysqli_query($conn, $query));
+                        var_dump($ativ);
+                        if ($ativ != NULL){
+                            echo("<option value=");
+                            echo("'".$ativ['id']."'");
+                            echo('>');
+                            echo$ativ['titulo'];
+                            echo("</option>");
+                        }
+                    $n -=1;
+                    //var_dump($rows);
+                    //var_dump($n);
+                    } 
+                    ?>
+                    
+                </select>
+                </div>
+                <div class="form-group">
+                    <label for="link" class="col-form-label">Link:</label>
+                    <textarea class="form-control" name="link" ></textarea>
+                </div>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancelar</button>
+                <button type="submit" class="btn btn-primary">Enviar</button>
+                </form>
+            </div>
+            </div>
+        </div>
+        </div>
 
 
     <h3>Aulas:</h3>
@@ -540,23 +625,13 @@ $_SESSION['disciplina'] =  $disciplina;
         
                          if (mysqli_num_rows(mysqli_query($conn, $sql)) != 0){
                             $atividade = mysqli_fetch_array(mysqli_query($conn, $sql)); 
+                            
                             $id=$atividade['id'];      
                             
                          ?>
                         <div class="card mb-1">
                             <div class="card-body ">
-                                <h4 class="accordion border "><?php echo($atividade['titulo'].' | '.$atividade['turma'])?>
-                                <div>
-                                <a><button type="button" class="btn btn-warning float-rigth " id="btnup" title="Subir Respostas"  data-toggle="modal" data-target="#exampleModal">
-                                            <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-cloud-arrow-up" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-                                                <path fill-rule="evenodd" d="M4.406 3.342A5.53 5.53 0 0 1 8 2c2.69 0 4.923 2 5.166 4.579C14.758 6.804 16 8.137 16 9.773 16 11.569 14.502 13 12.687 13H3.781C1.708 13 0 11.366 0 9.318c0-1.763 1.266-3.223 2.942-3.593.143-.863.698-1.723 1.464-2.383zm.653.757c-.757.653-1.153 1.44-1.153 2.056v.448l-.445.049C2.064 6.805 1 7.952 1 9.318 1 10.785 2.23 12 3.781 12h8.906C13.98 12 15 10.988 15 9.773c0-1.216-1.02-2.228-2.313-2.228h-.5v-.5C12.188 4.825 10.328 3 8 3a4.53 4.53 0 0 0-2.941 1.1z"/>
-                                                <path fill-rule="evenodd" d="M7.646 5.146a.5.5 0 0 1 .708 0l2 2a.5.5 0 0 1-.708.708L8.5 6.707V10.5a.5.5 0 0 1-1 0V6.707L6.354 7.854a.5.5 0 1 1-.708-.708l2-2z"/>
-                                            </svg>
-                                            </button>
-                                            </a>
-                                
-                                </div>
-                                </h4>
+                                <h4 class="accordion border "><?php echo($atividade['titulo'].' | '.$atividade['turma'])?></h4>
                                 <div class="panel">
                                     <h5 class="border-bottom">
                                         <a href="<?php echo($atividade['link'])?>">Baixe aqui os arquivos</a>
@@ -571,35 +646,6 @@ $_SESSION['disciplina'] =  $disciplina;
                                 
                             </div>
                         </div>
-                        
-                        <!-- Modal não está funcionando -->
-                        <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-                        <div class="modal-dialog">
-                            <div class="modal-content">
-                            <div class="modal-header">
-                                <h5 class="modal-title" id="exampleModalLabel">Subir Respostas atividade: <?php echo($atividade['titulo'].' | '.$atividade['turma'])?></h5>
-                                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                                <span aria-hidden="true">&times;</span>
-                                </button>
-                            </div>
-                            <div class="modal-body">
-                                <form method="POST" action="enviar-atividade.php?id=<?php echo$id?>">
-                                <label>Documento Resposta:</label><br>
-                                <textarea type="text-box"  name="link-resposta" placeholder="cole aqui o link dos documentos de resposta" rows="4" cols="50"></textarea>
-                                <br><input class="mt-1 btn btn-primary" value="Enviar" type="submit" name="btnCadUsuario" ><br>
-                                
-                                </form>
-                            </div>
-                            <div class="modal-footer">
-                                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-                                
-                            </div>
-                            </div>
-                        </div>
-                        </div>
-
-
-
                         <?php
                         
                         }
