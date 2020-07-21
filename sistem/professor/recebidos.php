d820e4194fc9e87d4ec8d433c935fb9c5491587a
Eliabe <eliabepaz.wprk@gmail.com>
2020-07-21 13:56:23 -0300

    design lista link

--- sistem/professor/recebidos.php
+++ sistem/professor/recebidos.php
@@ -7,16 +7,6 @@ $sql = "SELECT * FROM `atividade-aluno`";
 //echo $sql;
 $atividade = mysqli_fetch_array(mysqli_query($conn, $sql));
 //var_dump($atividade);
-
-$r = 100;
-while($r != 0){
-$sql = "SELECT * FROM `atividade-aluno` WHERE fila = " .$r. " AND disciplina = '" .$_SESSION['disciplina']. "'";
-//echo $sql;               
-       
-if (mysqli_num_rows(mysqli_query($conn, $sql)) != 0){
-    $respostas = mysqli_fetch_array(mysqli_query($conn, $sql));
-    //var_dump($respostas);
-    
 ?>
 <table class="table">
   <thead>
@@ -29,6 +19,17 @@ if (mysqli_num_rows(mysqli_query($conn, $sql)) != 0){
     </tr>
   </thead>
   <tbody>
+<?php
+$r = 100;
+while($r != 0){
+$sql = "SELECT * FROM `atividade-aluno` WHERE fila = " .$r. " AND disciplina = '" .$_SESSION['disciplina']. "'";
+//echo $sql;               
+       
+if (mysqli_num_rows(mysqli_query($conn, $sql)) != 0){
+    $respostas = mysqli_fetch_array(mysqli_query($conn, $sql));
+    //var_dump($respostas);
+    
+?>
     <tr>
       <th scope="row"><?php
         echo $respostas['titulo'];
@@ -45,17 +46,21 @@ if (mysqli_num_rows(mysqli_query($conn, $sql)) != 0){
         echo $respostas['turma'];
       ?></td>
       <td><?php
+        echo '<a href='."'";
         echo $respostas['link'];
+        echo "'".'>Dowload</a>';
       ?></td>
     </tr>
-    </tbody>
-</table>
 <?php
 }
 $r -=1;
 //var_dump($rows);
 //var_dump($r);
 }
+?>
+</tbody>
+</table>
+<?php
 
 
 
